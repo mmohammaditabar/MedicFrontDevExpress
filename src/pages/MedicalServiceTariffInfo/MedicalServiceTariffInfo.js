a407a02683e1ec74d64c7bcd063d046cce63c3f1
GemIran <GemIran@DESKTOP-JEFF4MU>
2023-02-21 12:10:52 +0330

    MedicalServiceTariff

---
+++ src/pages/MedicalServiceTariffInfo/MedicalServiceTariffInfo.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import DynamicDataGrid from '../../Base/Components/Grid/DynamicDataGrid';
+import { columnsMedicalService,getFormItems } from './Model/modelMedicalServiceTariff';
+
+
+
+function MedicalServiceTariffInfo() {
+  return (
+    <DynamicDataGrid url={'MedicalServiceTariffInfo'} columns={columnsMedicalService} getFormItems={getFormItems} />
+  );
+}
+
+export default MedicalServiceTariffInfo;
